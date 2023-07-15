fad4ac75a24efb6c207ce18cd47b7fcbb9206ed3
gaurav jain <gjain8883@gmail.com>
2023-07-15 17:06:32 +0530

    changes

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { IoFastFoodOutline } from "react-icons/io5";
 import '../styles/Loading.scss'
-import NotFOund from './NotFound';
 const Loading = () => {
 
   return (
